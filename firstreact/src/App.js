9121df76bb5899d243d5a0e4305dc838e322493c
Kenil Kanani <kenilkanani09@gmail.com>
2024-01-26 09:02:40 +0530

    Second Component Created.

---
+++ firstreact/src/App.js
@@ -0,0 +1,12 @@
+import Kenil from "./Second";
+
+function App() {
+  return (
+    <>
+      <Kenil/>
+      <h1>I am entering in thw world of React.</h1>
+    </>   
+  );
+}
+
+export default App;
