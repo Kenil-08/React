01b650912f27dbf0ccb11a86cf82b6cc0d79c400
Kenil Kanani <kenilkanani09@gmail.com>
2024-01-27 14:05:31 +0530

    change in code format.

--- counter-mini-project/src/App.jsx
+++ counter-mini-project/src/App.jsx
@@ -1,29 +1,31 @@
 // This mini Project allow you to increase/decrease between 0 to 20.
 
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  function addValue(){
-    if(count < 20)
+  function addValue() {
+    if (count < 20){
       setCount(count + 1);
+    }    
   }
-  function removeValue(){
-    if(count > 0)
+  function removeValue() {
+    if (count > 0){
       setCount(count - 1);
+    } 
   }
   return (
     <>
       <h1>Counter Mini Project</h1>
       <h3>Current Value : {count}</h3>
-      <button id='add' onClick={addValue}> Add Value {count}</button>
-      <button id='remove' onClick={removeValue} >Remove Value {count} </button>
+      <button id="add" onClick={addValue}>Add Value {count}</button>
+      <button id="remove" onClick={removeValue}>Remove Value {count}</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
