33f9e4bf5ae04bed3479af496e9e8c381c5f7bc6
kenil-08 <kenilkanani09@gmail.com>
2024-05-17 09:42:24 +0530

    Image Update

--- themeSwitcher/src/components/Card.jsx
+++ themeSwitcher/src/components/Card.jsx
@@ -6,7 +6,7 @@ export default function Card() {
       <a href="/">
         <img
           className="p-8 rounded-t-lg"
-          src="https://images.pexels.com/photos/18264716/pexels-photo-18264716/free-photo-of-man-people-laptop-internet.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          src="src/images/apple-watch-7.jpg"
           alt="product_image1"
         />
       </a>
