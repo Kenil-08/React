a5e0eb613beb4c91408f5da3aa21080992ee95b4
kenil-08 <kenilkanani09@gmail.com>
2024-06-05 18:07:14 +0530

    Store nd Slice Config

--- blog-site/src/App.jsx
+++ blog-site/src/App.jsx
@@ -1,11 +1,37 @@
 import './App.css'
+import React, { useState , useEffect} from 'react'
+import {useDispatch} from 'react-redux'
+import authService from './appwrite/auth'
+import {login, logout} from './features/authSlice'
+import {Header, Footer} from './components'
+import {Outlet} from 'react-router-dom'
 
 function App() {
-  return (
-    <>
-      <h1>Hello</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if(userData)
+          dispatch(login({userData}))
+        else
+          dispatch(logout())
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+        TODO:  <Outlet />
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
