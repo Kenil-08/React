c18fed22bfe666125d657f7d2033049949cffb74
kenil-08 <kenilkanani09@gmail.com>
2024-06-23 10:11:55 +0530

    Complete

--- blog-site/src/main.jsx
+++ blog-site/src/main.jsx
@@ -12,7 +12,8 @@ import Signup from './pages/Signup'
 import EditPost from './pages/EditPost'
 import Post from './pages/Post'
 import AllPosts from './pages/AllPosts'
-
+import { injectSpeedInsights } from '@vercel/speed-insights';
+injectSpeedInsights();
 const router = createBrowserRouter([
   {
     path: "/",
